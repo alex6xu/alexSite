1f42777ce439e444869e48755882dfcfb0e35b5c
alex <xuhui@weiyankeji.cn>
2019-06-12 18:46:22 +0800

    update blog

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,30 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 # from django.contrib.sites.models import Site
+import logging
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+from django.db import models
+from django.urls import reverse
+from django.conf import settings
+# from uuslug import slugify
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+# from DjangoBlog.utils import get_current_site
+# from DjangoBlog.utils import cache_decorator, cache
+from django.utils.functional import cached_property
+from django.utils.timezone import now
+# from mdeditor.fields import MDTextField
 
 # Create your models here.
+
+class Categoru(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_time = models.DateTimeField('创建时间', default=now)
+    last_mod_time = models.DateTimeField('修改时间', default=now)
+    name = models.CharField('分类名', max_length=30, unique=True)
+    parent_category = models.ForeignKey('self', verbose_name="父级分类", blank=True, null=True, on_delete=models.CASCADE)
+    slug = models.SlugField(default='no-slug', max_length=60, blank=True)
 
 class Tag(models.Model):
     tag_name = models.CharField(max_length=64)
@@ -16,24 +38,91 @@ class Tag(models.Model):
 
 
 class Article(models.Model):
-    title = models.CharField(max_length = 100)  #博客题目
-    author = models.ForeignKey(User, default=1, on_delete=models.SET_DEFAULT)
-    category = models.CharField(max_length = 50, blank = True)  #博客分类 可为空
-    tag = models.ManyToManyField(Tag, blank=True)  # 博客标签 可为空
-    date_time = models.DateTimeField(auto_now_add = True)  #博客日期
-    content = models.TextField(blank = True, null = True)  #博客文章正文
-    count = models.IntegerField(default=0)
+    """文章"""
+    STATUS_CHOICES = (
+        ('d', '草稿'),
+        ('p', '发表'),
+    )
+    COMMENT_STATUS = (
+        ('o', '打开'),
+        ('c', '关闭'),
+    )
+    TYPE = (
+        ('a', '文章'),
+        ('p', '页面'),
+    )
+    title = models.CharField('标题', max_length=200, unique=True)
+    body = MDTextField('正文')
+    pub_time = models.DateTimeField('发布时间', blank=True, null=True)
+    status = models.CharField('文章状态', max_length=1, choices=STATUS_CHOICES, default='p')
+    comment_status = models.CharField('评论状态', max_length=1, choices=COMMENT_STATUS, default='o')
+    type = models.CharField('类型', max_length=1, choices=TYPE, default='a')
+    views = models.PositiveIntegerField('浏览量', default=0)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='作者', on_delete=models.CASCADE)
+    article_order = models.IntegerField('排序,数字越大越靠前', blank=False, null=False, default=0)
+    category = models.ForeignKey('Category', verbose_name='分类', on_delete=models.CASCADE, blank=False, null=False)
+    tags = models.ManyToManyField('Tag', verbose_name='标签集合', blank=True)
 
-    def get_absolute_url(self):
-        path = reverse('detail', kwargs={'id':self.id})
-        return "http://127.0.0.1:8000%s" % path
+    def body_to_string(self):
+        return self.body
 
-    #python3使用__str__
-    def __str__(self) :
-        return self.title.encode('utf-8')
+    def __str__(self):
+        return self.title
 
     class Meta:
-        ordering = ['-date_time']
+        ordering = ['-article_order', '-pub_time']
+        verbose_name = "文章"
+        verbose_name_plural = verbose_name
+        get_latest_by = 'id'
+
+    def get_absolute_url(self):
+        return reverse('blog:detailbyid', kwargs={
+            'article_id': self.id,
+            'year': self.created_time.year,
+            'month': self.created_time.month,
+            'day': self.created_time.day
+        })
+
+    # @cache_decorator(60 * 60 * 10)
+    def get_category_tree(self):
+        tree = self.category.get_category_tree()
+        names = list(map(lambda c: (c.name, c.get_absolute_url()), tree))
+
+        return names
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def viewed(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    def comment_list(self):
+        cache_key = 'article_comments_{id}'.format(id=self.id)
+        value = cache.get(cache_key)
+        if value:
+            logger.info('get article comments:{id}'.format(id=self.id))
+            return value
+        else:
+            comments = self.comment_set.filter(is_enable=True)
+            cache.set(cache_key, comments, 60 * 100)
+            logger.info('set article comments:{id}'.format(id=self.id))
+            return comments
+
+    def get_admin_url(self):
+        info = (self._meta.app_label, self._meta.model_name)
+        return reverse('admin:%s_%s_change' % info, args=(self.pk,))
+
+    # @cache_decorator(expiration=60 * 100)
+    def next_article(self):
+        # 下一篇
+        return Article.objects.filter(id__gt=self.id, status='p').order_by('id').first()
+
+    # @cache_decorator(expiration=60 * 100)
+    def prev_article(self):
+        # 前一篇
+        return Article.objects.filter(id__lt=self.id, status='p').first()
+
 
 class Comment(models.Model):
     """
